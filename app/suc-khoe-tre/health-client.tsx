14a5797d3fd8d8c39f4c17d11fcbf4923d1afc19
BlueDragon33 <dinhnam331007@gmail.com>
2026-09-09 17:38:23 +0700

    Move age content out of the top-level shell

--- app/suc-khoe-tre/health-client.tsx
+++ app/suc-khoe-tre/health-client.tsx
@@ -2,14 +2,10 @@
 
 import HealthFramework, { type HealthDeviceAccess } from "./health-framework";
 import HealthFrameworkMap from "./health-framework-map";
-import AgeContentCenter from "./age-content-center";
 
 export default function HealthClient({ initialCourse, device }: { initialCourse: unknown; device: HealthDeviceAccess }) {
   return <>
     <HealthFrameworkMap />
-    <div className="ac-shell-dock">
-      <AgeContentCenter />
-    </div>
     <HealthFramework initialCourse={initialCourse} device={device} />
   </>;
 }
