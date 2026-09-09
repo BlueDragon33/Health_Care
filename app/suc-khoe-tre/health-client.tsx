a9648e4e5ad213a5b2b24e7766e74455485fbd91
BlueDragon33 <dinhnam331007@gmail.com>
2026-09-09 16:32:49 +0700

    Expose restored age content center in health app

--- app/suc-khoe-tre/health-client.tsx
+++ app/suc-khoe-tre/health-client.tsx
@@ -2,10 +2,14 @@
 
 import HealthFramework, { type HealthDeviceAccess } from "./health-framework";
 import HealthFrameworkMap from "./health-framework-map";
+import AgeContentCenter from "./age-content-center";
 
 export default function HealthClient({ initialCourse, device }: { initialCourse: unknown; device: HealthDeviceAccess }) {
   return <>
     <HealthFrameworkMap />
+    <div className="ac-shell-dock">
+      <AgeContentCenter />
+    </div>
     <HealthFramework initialCourse={initialCourse} device={device} />
   </>;
 }
