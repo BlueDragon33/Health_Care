6a3741d41fb1b9a145d05e39907b98933e35385b
BlueDragon33 <dinhnam331007@gmail.com>
2026-09-09 08:42:14 +0700

    feat(modules): mount advanced Health modules in canonical client

--- app/suc-khoe-tre/health-client.tsx
+++ app/suc-khoe-tre/health-client.tsx
@@ -2,10 +2,12 @@
 
 import HealthFramework, { type HealthDeviceAccess } from "./health-framework";
 import HealthFrameworkMap from "./health-framework-map";
+import AdvancedHealthModules from "./advanced-health-modules";
 
 export default function HealthClient({ initialCourse, device }: { initialCourse: unknown; device: HealthDeviceAccess }) {
   return <>
     <HealthFrameworkMap />
+    <AdvancedHealthModules />
     <HealthFramework initialCourse={initialCourse} device={device} />
   </>;
 }
